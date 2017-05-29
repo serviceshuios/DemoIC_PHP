756fbc6edad5ea38a96c7abfde96bca67e15e74d
Arnaud Brugnon <arnaud.brugnon@decasoft.fr>
2017-05-29 16:31:54 +0200

    Correction d'un oubli de ; dans le fichier sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,9 +1,9 @@
 <?php
 
-echo "<div class='sidebar_left'>"
+echo "<div class='sidebar_left'>";
 	
 $links = array("Lien 1" => "/page-1", "Lien 2" => "/page-2", "Lien 3" => "/page-3");
 foreach($links as $title => $link){
 	echo "<a title='".$title."' href='".$link."'>".$title."</a>";
 }
-echo "</div>"
+echo "</div>":
